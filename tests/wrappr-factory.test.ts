9bd4af01bd603f7d4f7b3f8631e7d1a02a6637cb
shiv <shivanshityagi@icloud.com>
2022-07-29 16:18:14 +0000

    update deployWrappr test

--- tests/wrappr-factory.test.ts
+++ tests/wrappr-factory.test.ts
@@ -7,33 +7,16 @@ import {
   afterAll
 } from "matchstick-as/assembly/index"
 import { Address, BigInt } from "@graphprotocol/graph-ts"
-import { ExampleEntity } from "../generated/schema"
-import { WrapprDeployed } from "../generated/WrapprFactory/WrapprFactory"
-import { handleWrapprDeployed } from "../src/wrappr-factory"
-import { createWrapprDeployedEvent } from "./wrappr-factory-utils"
+import { Wrappr } from "../generated/schema"
+import { handleWrapprDeployed } from '../src/wrappr-factory';
+import { createWrapprDeployedEvent } from './wrappr-factory-utils'
 
 // Tests structure (matchstick-as >=0.5.0)
 // https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
-
 describe("Describe entity assertions", () => {
   beforeAll(() => {
-    let wrappr = Address.fromString(
-      "0x0000000000000000000000000000000000000001"
-    )
-    let name = "Example string value"
-    let symbol = "Example string value"
-    let baseURI = "Example string value"
-    let mintFee = BigInt.fromI32(234)
-    let admin = Address.fromString("0x0000000000000000000000000000000000000001")
-    let newWrapprDeployedEvent = createWrapprDeployedEvent(
-      wrappr,
-      name,
-      symbol,
-      baseURI,
-      mintFee,
-      admin
-    )
-    handleWrapprDeployed(newWrapprDeployedEvent)
+    let wrapprDeploymentEvent = createWrapprDeployedEvent(Address.fromString("0xa16081f360e3847006db660bae1c6d1b2e17ec2a"), 'Test Wrappr', 'TEST', 'ipfs://mock', BigInt.fromI32(234), Address.fromString("0x0000000000000000000000000000000000000002"))
+    handleWrapprDeployed(wrapprDeploymentEvent)
   })
 
   afterAll(() => {
@@ -43,45 +26,39 @@ describe("Describe entity assertions", () => {
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
 
-  test("ExampleEntity created and stored", () => {
-    assert.entityCount("ExampleEntity", 1)
+  test("Wrappr created and stored", () => {
+    assert.entityCount("Wrappr", 1)
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
     assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
-      "wrappr",
-      "0x0000000000000000000000000000000000000001"
-    )
-    assert.fieldEquals(
-      "ExampleEntity",
+      "Wrappr",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
       "name",
-      "Example string value"
+      "Test Wrappr"
     )
     assert.fieldEquals(
-      "ExampleEntity",
+      "Wrappr",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
       "symbol",
-      "Example string value"
+      "TEST"
     )
     assert.fieldEquals(
-      "ExampleEntity",
+      "Wrappr",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
       "baseURI",
-      "Example string value"
+      "ipfs://mock"
     )
     assert.fieldEquals(
-      "ExampleEntity",
+      "Wrappr",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
       "mintFee",
       "234"
     )
     assert.fieldEquals(
-      "ExampleEntity",
+      "Wrappr",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
       "admin",
-      "0x0000000000000000000000000000000000000001"
+      "0x0000000000000000000000000000000000000002"
     )
 
     // More assert options:
