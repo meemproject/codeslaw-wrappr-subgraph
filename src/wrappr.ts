59f4cc8aa5ed0676121193ede6520cf8860c0b1c
shiv <shivanshityagi@icloud.com>
2022-08-23 12:10:21 +0000

    update transfer

--- src/wrappr.ts
+++ src/wrappr.ts
@@ -57,6 +57,7 @@ export function handleDelegateVotesChanged(event: DelegateVotesChanged): void {}
 export function handleManagerSet(event: ManagerSet): void {
   const manager = new Manager(event.address.toHexString() + event.params.to.toHexString())
 
+  manager.wrappr = event.address.toHexString()
   manager.address = event.params.to
   manager.active = event.params.set
 
@@ -74,6 +75,7 @@ export function handleMintFeeSet(event: MintFeeSet): void {
 export function handleOwnerOfSet(event: OwnerOfSet): void {
   const collection = new Collection(event.address.toHexString() + event.params.id.toHexString())
 
+  collection.wrappr = event.address.toHexString()
   collection.collectionId = event.params.id
   collection.owner = event.params.to
   
@@ -83,6 +85,7 @@ export function handleOwnerOfSet(event: OwnerOfSet): void {
 export function handlePermissionSet(event: PermissionSet): void {
   const collection = new Collection(event.address.toHexString() + event.params.id.toHexString())
 
+  collection.wrappr = event.address.toHexString()
   collection.collectionId = event.params.id
   collection.transferability = event.params.set
   
@@ -95,44 +98,53 @@ export function handleTransferBatch(event: TransferBatch): void {}
 // REVIEW
 export function handleTransferSingle(event: TransferSingle): void {
   const collection = new Collection(event.address.toHexString() + event.params.id.toHexString())
+  collection.wrappr = event.address.toHexString()
   collection.collectionId = event.params.id
 
   if (event.params.from.toHexString() == AddressZero) {  // mint
     collection.transferability = false
     collection.permissioned = false
-    const user = new User(event.address.toHexString() + event.params.id.toHexString() + event.params.to.toHexString())
+    let user = User.load(event.address.toHexString() + event.params.id.toHexString() + event.params.to.toHexString());
     
-    if (user.amount === null) {
+    if (user === null) {
+      user = new User(event.address.toHexString() + event.params.id.toHexString() + event.params.to.toHexString());
       user.amount = BigInt.fromI32(0)
-      user.amount = user.amount.plus(event.params.amount)
     } 
+    
+    user.collection = event.address.toHexString() + event.params.id.toHexString()
     user.amount = user.amount.plus(event.params.amount)
 
     user.save()
   } else if (event.params.to.toHexString() == AddressZero) { // burn 
-    const user = new User(event.address.toHexString() + event.params.id.toHexString() + event.params.to.toHexString())
+    let user = User.load(event.address.toHexString() + event.params.id.toHexString() + event.params.to.toHexString())
     
-      if (user.amount === null) {
+      if (user === null) {
+        user = new User(event.address.toHexString() + event.params.id.toHexString() + event.params.to.toHexString())
         user.amount = BigInt.fromI32(0)
       }
 
+      user.collection = event.address.toHexString() + event.params.id.toHexString()
       user.amount = user.amount.minus(event.params.amount)
 
       user.save()
   } else { // member to member
-    const userFrom = new User(event.address.toHexString() + event.params.id.toHexString() + event.params.from.toHexString())
-    const userTo = new User(event.address.toHexString() + event.params.id.toHexString() + event.params.to.toHexString())
+    let userFrom = new User(event.address.toHexString() + event.params.id.toHexString() + event.params.from.toHexString())
+    let userTo = new User(event.address.toHexString() + event.params.id.toHexString() + event.params.to.toHexString())
 
-    if (userTo.amount === null) {
+    if (userTo === null) {
+      userTo = new User(event.address.toHexString() + event.params.id.toHexString() + event.params.to.toHexString())
       userTo.amount = BigInt.fromI32(0)
     }
 
+    userTo.collection = event.address.toHexString() + event.params.id.toHexString()
     userTo.amount = userTo.amount.plus(event.params.amount)
 
-    if (userFrom.amount === null) {
+    if (userFrom === null) {
+      userFrom = new User(event.address.toHexString() + event.params.id.toHexString() + event.params.from.toHexString())
       userFrom.amount = BigInt.fromI32(0)
     }
 
+    userFrom.collection = event.address.toHexString() + event.params.id.toHexString()
     userFrom.amount = userFrom.amount.minus(event.params.amount)
 
     userTo.save()
@@ -145,6 +157,7 @@ export function handleTransferSingle(event: TransferSingle): void {
 export function handleTransferabilitySet(event: TransferabilitySet): void {
   const collection = new Collection(event.address.toHexString() + event.params.id.toHexString())
 
+  collection.wrappr = event.address.toHexString()
   collection.collectionId = event.params.id
   collection.transferability = event.params.set
   
@@ -154,6 +167,7 @@ export function handleTransferabilitySet(event: TransferabilitySet): void {
 export function handleURI(event: URI): void {
   const collection = new Collection(event.address.toHexString() + event.params.id.toHexString())
 
+  collection.wrappr = event.address.toHexString()
   collection.uri = event.params.value 
 
   collection.save()
